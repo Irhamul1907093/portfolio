1e08a6438498a726c80a78a1e1fc5dfb2ad95933
Irhamul1907093 <irhamulislam.pranjal@gmail.com>
2025-05-17 22:32:16 +0600

    almost_done_portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+<script>
+  const backToTopButton = document.getElementById("backToTop");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  };
+
+  backToTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+</script>
